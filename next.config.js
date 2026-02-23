111c6536850337f94975085e6c4078f928122487
Vivaan Jhaveri <vivaan.jhaveri@gmail.com>
2026-02-22 21:48:57 -0800

    Update Next.js to v16 with Turbopack config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
 	devIndicators: false,
 	images: { unoptimized: true },
-	webpack: (config, { isServer }) => {
-		// Disable cache for both client and server builds
-		config.cache = false;
-		return config;
-	},
-	// Add experimental features to handle client pages properly
-	experimental: {
-		appDir: true,
-		serverActions: true
-	}
+	turbopack: {},
 };
 
 module.exports = nextConfig;
